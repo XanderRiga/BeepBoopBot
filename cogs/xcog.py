6db6f9272cf528b54545059cec84e9ae06d0b47b
Xander Riga <Xander Riga>
2017-03-01 17:31:29 -0800

    attempted to create an hours played function

--- cogs/xcog.py
+++ cogs/xcog.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import urllib
+from urllib.request import urlopen
 
 try: # check if BeautifulSoup4 is installed
 	from bs4 import BeautifulSoup
@@ -40,6 +42,34 @@ class Mycog:
             await self.bot.say(online + ' players are playing Rocket League at the moment')
         except:
            await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+
+
+    @commands.command()
+    async def hours(self, user):
+        """Returns hours played by a user"""
+
+        #link = user
+        #substringCheck = "https://steamcommunity.com/profiles/"
+        #substringCheck2 = "https://steamcommunity.com/id/"
+        #if substringCheck in link:
+        #    url = link + "/games/?tab=all"  # build the web address
+        #if substringCheck2 in link:
+        #    url = link + "/games/?tab=all"  # build the web address
+        #else:
+        #    url = substringCheck2 + link + "/games/?tab=all"
+
+        url = "http://steamcommunity.com/id/xanderdagr8/games/?tab=all"
+
+        #await self.bot.say(url)
+        async with aiohttp.get(url) as response:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+            #await self.bot.say(soup.renderContents())
+        try:
+            name_box = soup.find('h5').get_text()
+            await self.bot.say(name_box)
+        except:
+           await self.bot.say("This user does not own Rocket League")
+
 
 
 def setup(bot):
