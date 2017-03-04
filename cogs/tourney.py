8cca561145e22961e9704a5606cc4dc3d8212839
Patrick <farrellryan@comcast.net>
2017-03-04 13:37:24 -0800

    Challonge.py edits

--- cogs/tourney.py
+++ cogs/tourney.py
@@ -1,21 +1,21 @@
 #import challonge
-import discord
-from discord.ext import commands
-import discord.utils
+#import discord
+#from discord.ext import commands
+#import discord.utils
 
 class tourney:
 
 	def __init__(self, bot):
 		self.bot = bot
 		self.APIkey = "CgfNVgAJM8x8IMr8dEKcKBPxEilyLKR87sGYaCD5"; #from challonge
-		challonge.set_credentials("Dunkas", self.APIkey)
+		#challonge.set_credentials("Dunkas", self.APIkey)
 
 	#create the tourney
 	@commands.command(pass_context=True)
 	async def createtourney(self, name):
-		challonge.tournaments.start(name)
-		tournament = challonge.tournaments.show(name)
-		await self.bot.say( tournament )
+		#challonge.tournaments.start(name)
+		#tournament = challonge.tournaments.show(name)
+		await self.bot.say( "here" )
 
 	#allow people to enter the tourney
 	@commands.command(pass_context=True)
