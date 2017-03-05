ceffa79f673e5e7f245254c5b3f55f87f2be679a
Patrick <farrellryan@comcast.net>
2017-03-04 17:32:30 -0800

    challonge

--- cogs/tourney.py
+++ cogs/tourney.py
@@ -3,30 +3,34 @@ import discord
 from discord.ext import commands
 import discord.utils
 
-challonge.set_credentials("Dunkas", "CgfNVgAJM8x8IMr8dEKcKBPxEilyLKR87sGYaCD5")
-
 class tourney:
 
 	def __init__(self, bot):
 		self.bot = bot
-		self.APIkey = "CgfNVgAJM8x8IMr8dEKcKBPxEilyLKR87sGYaCD5"; #from challonge
 
 	#create the tourney
 	@commands.command(pass_context=True)
 	async def createtourney(self, name):
-		challonge.set_credentials("Dunkas", self.APIkey)
-		challonge.tournaments.start(name)
-		tournament = challonge.tournaments.show(name)
+		# Tell pychallonge about your [CHALLONGE! API credentials](http://api.challonge.com/v1).
+		challonge.set_credentials("Dunkas", "CgfNVgAJM8x8IMr8dEKcKBPxEilyLKR87sGYaCD5")
 
-	#allow people to enter the tourney
-	@commands.command(pass_context=True)
-	async def jointourney(self):
-		await self.bot.say("method not defined")
+		# Retrieve a tournament by its id (or its url).
+		tournament = challonge.tournaments.show(3272)
 
-	#create a bracket and begin the tourney
-	@commands.command(pass_context=True)
-	async def starttourney(self):
-		await self.bot.say("method not defined")
+		# Tournaments, matches, and participants are all represented as normal Python dicts.
+		print(tournament["id"])  # 3272
+		print(tournament["name"])  # My Awesome Tournament
+		print(tournament["started-at"])  # None
+
+		# Retrieve the participants for a given tournament.
+		participants = challonge.participants.index(tournament["id"])
+		print(len(participants))  # 13
+
+		# Start the tournament and retrieve the updated information to see the effects
+		# of the change.
+		challonge.tournaments.start(tournament["id"])
+		tournament = challonge.tournaments.show(tournament["id"])
+		print(tournament["started-at"])  # 2011-07-31 16:16:02-04:00
 
 def setup(bot):
 	bot.add_cog(tourney(bot))
